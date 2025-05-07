846981f91a9c2bb2d3039479142d7132e5ff0a84
Rostya <rostyamudrik@proton.me>
2025-05-07 21:30:20 +0200

    hosts updated

--- client_crm/settings.py
+++ client_crm/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = SK
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 AUTH_USER_MODEL = 'accounts.User'  # или как ты назвал модель
 
