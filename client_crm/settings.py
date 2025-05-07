e8ccb97e781f55128d2243cc8a323fb75267a4b6
Rostya <rostyamudrik@proton.me>
2025-05-07 21:00:06 +0200

    Some changes

--- client_crm/settings.py
+++ client_crm/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from config import SK
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6#p*673f)_^v62$4_f)=m1cod=(wio@tr9$gejf-wdc#=f+c5'
+SECRET_KEY = SK
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
